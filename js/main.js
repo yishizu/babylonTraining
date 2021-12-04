363c22e96107466be7db70555688399c57f54051
yishizu <48244588+yishizu@users.noreply.github.com>
2021-12-04 17:54:32 +0900

    Add materials

--- js/main.js
+++ js/main.js
@@ -18,10 +18,13 @@ const engine = new BABYLON.Engine(canvas, true);
     // create light
     const ligth = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0),scene);
     // create box
-    const  box = BABYLON.MeshBuilder.CreateBox("box", {size: 1}, scene);
+    const box = BABYLON.MeshBuilder.CreateBox("box", {size: 1}, scene);
+    box.rotation.z = 3;
+    box.rotation.x = 3;
     // create sphere
     const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 1 ,segments:32}, scene);
     sphere.position = new BABYLON.Vector3(3,0,0);
+    sphere.scaling = new BABYLON.Vector3(1,2,1);
     // create plane
     const plane = BABYLON.MeshBuilder.CreatePlane("plane", {size: 1}, scene);
     plane.position = new BABYLON.Vector3(-3,0,0);
@@ -32,6 +35,18 @@ const engine = new BABYLON.Engine(canvas, true);
         new BABYLON.Vector3(0,1,0),
     ];
     const lines = BABYLON.MeshBuilder.CreateLines("lines", {points}, scene);
+
+    //create materials
+    const material1 = new BABYLON.StandardMaterial('material', scene);
+    material1.diffuseColor = new BABYLON.Color3(1,0,0);
+    material1.emissiveColor = new BABYLON.Color3(0.5,0.5,0);
+    box.material = material1;
+    const material2 = new BABYLON.StandardMaterial('material2', scene);
+    material2.diffuseColor = new BABYLON.Color3(1,0,1);
+    sphere.material = material2;
+    const material3 = new BABYLON.StandardMaterial('material3', scene);
+    material3.diffuseTexture = new BABYLON.Texture('assets/image.png')
+    plane.material = material3;
 
 
     return scene;
